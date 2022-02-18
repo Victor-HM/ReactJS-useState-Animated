15eb6ad23188b0678114d37a0e8138646d06cc93
Victor-HM <victor.27cm.santos@gmail.com>
2022-02-17 22:53:25 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [position, setPosition] = useState(new Animated.Value(100));
+  const [opacity, setOpacity] = useState(new Animated.Value(0));
+
+  Animated.sequence([
+    Animated.timing(
+      position,
+      {
+        toValue: 0,
+        duration: 1000,
+      }
+    ).start(),
+    Animated.timing(
+      opacity,
+      {
+        toValue: 1,
+        duration: 1000
+      }
+    ).start()
+  ])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Animated.View style={{
+        width: '80%',
+        height: 200,
+        backgroundColor: '#999',
+        borderRadius: 8,
+        justifyContent: 'center', 
+        alignItems: 'center',
+        top: position,
+        opacity: opacity
+      }}>
+        <Text>Animação usando Animated e useState</Text>
+      </Animated.View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +49,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
